9747f546542f466096d8ebb85372f98de410552e
KeikooUwU <127074149+KeikooUwU@users.noreply.github.com>
2023-03-23 15:28:00 +0700

    4th procedure

--- InsertionSort/InsertionSort/InsertionSort.cpp
+++ InsertionSort/InsertionSort/InsertionSort.cpp
@@ -18,16 +18,51 @@ void input()
         }
         else
         {
-            cout << "\nArray yang anda masukan maksimal 20 elemen. \n";   // Menampilkan pesan jika lebih dari dua puluh
+            cout << "\nArray yang anda masukan maksimal 20 elemen. \n";   // Menampilkan pesan jika lebih dari 20
         }
     }
     cout << endl;                                       // Membuat jarak per baris program
     cout << "======================" << endl;           // Membuat tampilan susunan data element array
     cout << "Masukkan Element Array" << endl;
-    cout << "======================" << endl; 
+    cout << "======================" << endl;
 
-    for (int i = o; i < n; i++)                         // Menggunakan perulangan for untuk menyimpan data pada array
+    for (int i = 0; i < n; i++)                         // Menggunakan perulangan for untuk menyimpan data pada array
     {
+        cout << "Data ke-" << (i + 1) << ": ";          // Memasukkan atau menginputkan nilai pada data n
+        cin >> arr[i];                                  // Memasukkan nilai data n kedalam Array
+    }
+};
+// Procedure InsertionSort
+void insertionsort()
+{
+
+    int temp; // Membuat Variabel data temporer atau penyimpanan sementara
+    int j;    // Membuat variabel j sebagai penanda 
+
+    for (int i = 1; i < n; i++) // 1. Looping dengan i dimulai dari 1 hingga n-1
+    {
+
+        temp = arr[i]; // 2. Simpan nilai arr[i] ke variabel sementara temp
+
+        j = i - 1; // 3. Setting nilai j sama dengan i-1;
+
+        while (j >= 0 && arr[j] > temp) // 4. Looping while dimana nilai j lebih besar sama dengan 0 dan array lebih besar daripada temp
+        {                               // arr[j] lebih besar daripada temp
+            arr[j + 1] = arr[j];        // a. simpan arr[j] ke dalam variabel arr[j + 1}
+            j--;                        // b. Decrement nlai j by 1
+        }
+
+        arr[j + 1] = temp;              // 5. simpan nilai temp ke dalam arr[j + 1]
+
+
+        cout << endl;
+        cout << "\nPass " << i << ": "; // Output ke layar 
+        for (int k = 0; k < n; k++)
+        {                               // Looping nilai k cimiulai dari 0 hingga n-1
+            cout << arr[k] << " ";      // Output kr layar
+        }
+    }
+};
 
 
 
